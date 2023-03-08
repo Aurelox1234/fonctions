ea7eb83f7410507279fc92a85fc813587b60c2f9
aurelox1234 <adubois1307@gmail.com>
2023-03-08 17:34:08 +0100

    cours 1 sur fonctions

---
+++ index.js
@@ -0,0 +1,61 @@
+// exercice page 2
+
+// function getReminder() {
+// console.log ("Arroser les plantes")
+// }
+// function greetInSpanish() {
+//     console.log("Buenas tardes!")
+// }
+// getReminder();
+// greetInSpanish();
+
+//exercice page 3
+
+// function sayThanks(name) {
+//     console.log('Merci pour votre achat ' + name  + '! Nous apprécions votre entreprise.')
+// }
+// sayThanks();
+// sayThanks();
+// sayThanks();
+
+//exercice page 4
+
+// sayThanks("Cole");
+
+//exercice page 5
+
+// function makeShoppingList(item1 = "du lait",item2 = "du pain", item3 = "des oeufs") {
+// console.log(`Acheter ${item1}`);
+// console.log(`Acheter ${item2}`);
+// console.log(`Acheter ${item3}`);
+// }
+// makeShoppingList()
+
+//exercice page 6
+
+// function monitorCount(lignes, colonnes) {
+//     return lignes * colonnes
+// }
+
+// const numOfMonitors = monitorCount(5, 4)
+// console.log(numOfMonitors)
+
+// //exrecice page 7
+
+// function costOfMonitors(lignes, colonnes) {
+//     return monitorCount(lignes, colonnes) * 200
+// }
+// const totalCost = costOfMonitors(5, 4)
+
+// console.log(totalCost)
+
+//exercice page 8
+
+// const plantNeedsWater = function(jour) {
+//     if(jour === "Mercredi"){
+//         return console.log(true + " Active la led")
+//     } else {
+//         return console.log(false)
+//     }
+// }
+// plantNeedsWater("Mercredi")
